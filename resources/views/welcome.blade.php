ef916ba0996ee889ce81e2e3f058e2909dbfc30e
anttieek <an310892an>
2017-09-25 14:30:30 +0300

    jou

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Jeejeeejoujou
                 </div>
 
                 <div class="links">
