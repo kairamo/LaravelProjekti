6f96d77c38e520cf1e84fa3223ce736c58baf0eb
Joonas Järvinen <jarvinen.joonass@gmail.com>
2017-09-25 13:30:17 +0300

    moi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Jeejeee
                 </div>
 
                 <div class="links">
