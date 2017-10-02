01ae3b8853d0485fab2c25f70b4c864ea4644dbb
anttieek <an310892an>
2017-10-02 13:11:22 +0300

    jou

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('id={id}', 'ravintolatController@searchById');
 
 Route::get('/all', 'ravintolatController@showAll');
 
+Route::get('ravintola={ravintola}', 'ravintolatController@searchByName');
+
+Route::get('kaupunki={paikka}', 'ravintolatController@searchByLocation');
+
 
 
 
